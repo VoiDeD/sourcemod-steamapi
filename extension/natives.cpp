4fe21279e8896048e07ec00b2d95c90462af9ac6
Ryan Stecker <voidedweasel@gmail.com>
2014-02-08 23:38:29 -0600

    Implement SteamAPI_GetClientSteamID.

--- extension/natives.cpp
+++ extension/natives.cpp
@@ -22,9 +22,28 @@ static cell_t Native_SetGameDescription( IPluginContext *pContext, const cell_t 
 	return 0;
 }
 
+static cell_t Native_GetClientSteamID( IPluginContext *pContext, const cell_t *params )
+{
+	const CSteamID *pSteamID = engine->GetClientSteamIDByPlayerIndex( params[ 1 ] );
+
+	if ( !pSteamID )
+	{
+		return pContext->ThrowNativeError( "Invalid or unconnected player!" );
+	}
+
+	char *steamIdBuffer;
+	pContext->LocalToString( params[ 2 ], &steamIdBuffer );
+
+	int numBytes = g_pSM->Format( steamIdBuffer, params[ 3 ], "%llu", pSteamID->ConvertToUint64() );
+	numBytes++; // account for null terminator
+	
+	return numBytes;
+}
+
 
 sp_nativeinfo_t g_Natives[] =
 {
 	{ "SteamAPI_SetGameDescription", Native_SetGameDescription },
+	{ "SteamAPI_GetClientSteamID", Native_GetClientSteamID },
 	{ NULL, NULL }
 };
